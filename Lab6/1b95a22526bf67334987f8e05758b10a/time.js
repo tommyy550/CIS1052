a8091e5f3ff80debf4ab2d50c64adbbf5229b889
Tommy Huynh <tommyhuynh64@gmail.com>
2020-10-31 16:32:56 -0400

    lab 6

---
+++ Lab6/1b95a22526bf67334987f8e05758b10a/time.js
@@ -0,0 +1,15 @@
+﻿
+
+function rotateSeconds(){
+    let sec = document.querySelector("#second");
+    sec.style.transform = "rotate(30deg)";
+}
+
+// setInterval(rotateSeconds,100);
+let sec=0;
+while(sec<60){
+    let x = document.querySelector("#second");
+    x.style.transform = "rotate("++"deg)";
+    sec=sec+6;
+}
+
---
+++ Lab6/js.js
@@ -0,0 +1,33 @@
+﻿function sevenInFinal(num){
+    let lastDigit=num%10;
+    return lastDigit===7;
+}
+
+function toUpperCase(str){
+    return str.toUpperCase();
+}
+
+function lengthOfPalindrome(str){
+    return str.length;
+}
+
+function isPalindrome(str){
+    var newString = "";
+    for (i = str.length - 1; i >= 0; i--) { 
+        newString += str[i];
+    }
+    return str===newString;
+}
+
+function sevenInFinal2(arr){
+    return arr.filter(sevenInFinal);
+}
+
+function toUpperCase2(arr){
+    return arr.map(toUpperCase);
+}
+
+function lengthOfPalindrome2(arr){
+    newArr=arr.filter(isPalindrome);
+    return newArr.map(lengthOfPalindrome);
+}
