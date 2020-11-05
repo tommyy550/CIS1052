d5ee2aa6cfc1bcff861ebc1992dc8035090fbb53
tommy <tommyhuynh64@gmail.com>
2020-11-05 15:07:35 -0500

    pokedex

---
+++ Lab6/Part2/js.js
@@ -0,0 +1,44 @@
+﻿function separate(arr,func){
+    let arrTrue=[];
+    let arrFalse=[];
+    for(const element of arr){
+        if(func(element)){
+            arrTrue.push(element);
+        }
+        else{
+            arrFalse.push(element);
+        }
+    }
+    let retval=[];
+    retval.push(arrTrue);
+    retval.push(arrFalse);
+    return retval;
+}
+
+function isEven(num){
+    if(num%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+separate([1,2,3,4,5,6], isEven)
+
+function bestOfBothWorlds(arr1,arr2,func){
+    let retval=[];
+    for(let i=0;i<arr1.length;i++){
+        retval.push(func(arr1[i],arr2[i]))
+    }
+    return retval;
+}
+
+function biggerIsBetter(num1,num2){
+    if (num1>num2){
+        return num1;
+    }
+    return num2;
+}
+
+bestOfBothWorlds([1,11,2], [15, 7, 5], biggerIsBetter)
---
+++ Lab6/Part3/time.js
@@ -0,0 +1,59 @@
+﻿function doInitial(){
+    var date = new Date;
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hours = date.getHours()%12;
+
+    console.log("sseconds"+seconds+"minutes"+minutes+"hours"+hours);
+
+    let sec = document.querySelector("#second");
+    sec.style.transform = "rotate("+ 6*seconds +"deg)";
+    secDegree=6*seconds;
+
+    let min = document.querySelector("#minute");
+    //the minute hand should move 360/(60*60) degrees per second
+    minDegree=(360/(60*60))*(60*minutes+seconds);
+    min.style.transform = "rotate("+ minDegree +"deg)";
+    
+
+    let hr = document.querySelector("#hour");
+    //the hour hand should move 360/(3600*12) degrees per second
+    hrDegree=(360/(3600*12))*(hours*3600+seconds+minutes*60);
+    hr.style.transform = "rotate("+  hrDegree +"deg)";
+    
+}
+
+function rotateSeconds(){
+    secDegree+=6;
+    let sec = document.querySelector("#second");
+    sec.style.transform = "rotate("+ secDegree +"deg)";
+}
+
+function rotateMinutes(){
+    minDegree+=360/(60*60);
+    let min = document.querySelector("#minute");
+    min.style.transform = "rotate("+ minDegree +"deg)";
+}
+
+function rotateHours(){
+    hrDegree+=360/(3600*12);
+    let hr = document.querySelector("#hour");
+    hr.style.transform = "rotate("+ hrDegree +"deg)";
+}
+
+
+let secDegree=0;
+let minDegree=0;
+let hrDegree=0;
+doInitial();
+setInterval(rotateSeconds,1000);
+setInterval(rotateMinutes,1000);
+setInterval(rotateHours,1000);
+// console.log(secDegree);
+// console.log(minDegree);
+// console.log(hrDegree);
+
+
+
+
+
