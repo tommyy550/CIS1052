a6f62a06cd90397fcb0c6f3046af31d89c01fb91
tommy <tommyhuynh64@gmail.com>
2020-12-02 13:37:32 -0500

    la9

---
+++ Lab9/Part1/pokemon.js
@@ -0,0 +1,173 @@
+﻿function getPokemonInfo() {
+    //get user input
+    let search = document.querySelector("#search");
+    let pokemon = search.value.toLowerCase().trim();
+
+    //console.log(pokemon);
+
+    //clear fields
+    reset();
+
+    fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
+        .then(resp => {
+            return resp.json()
+        })
+        .then(data => {
+            console.log(data);
+            
+
+            const name = data.name;
+            document.querySelector("#name").innerHTML = name;
+
+            // if(document.querySelector("#name").innerText == null){
+            //     alert("error");
+            //     document.querySelector("#name").innerHTML = "Pokemon does not exist."
+            // }
+
+            const image = data.sprites.front_default;
+            document.querySelector("#image").innerHTML = "<img src='" + image + "'/>";
+
+            const height = data.height;
+            document.querySelector("#height").innerHTML = height;
+
+            const weight = data.weight;
+            document.querySelector("#weight").innerHTML = weight;
+
+            //types
+            document.querySelector("#types").innerHTML = "";
+            const types = data.types.map(types => types.type);
+            for (const type of types) {
+                document.querySelector("#types").innerHTML += type.name + " ";
+            }
+
+            //stats
+            document.querySelector("#hp").innerHTML = data.stats[0].base_stat;
+            document.querySelector("#attack").innerHTML = data.stats[1].base_stat;
+            document.querySelector("#defense").innerHTML = data.stats[2].base_stat;
+            document.querySelector("#special-attack").innerHTML = data.stats[3].base_stat;
+            document.querySelector("#special-defense").innerHTML = data.stats[4].base_stat;
+            document.querySelector("#speed").innerHTML = data.stats[5].base_stat;
+
+            barChart(data.stats[0].base_stat,data.stats[1].base_stat,data.stats[2].base_stat,data.stats[3].base_stat,data.stats[4].base_stat,data.stats[5].base_stat);
+
+
+            //move list
+            const myList = document.querySelector("#move-list");
+            const moves = data.moves.map(moves => moves.move);
+            for (const move of moves) {
+
+                var listElement = document.createElement("LI");
+                listElement.innerHTML += move.name;
+
+                var button = document.createElement('button');
+                button.innerHTML = 'Get More Info';
+                button.className="btn btn-primary btnPadding";
+
+                button.onclick = function () {
+                    fetch(move.url, { headers: { "Accept": "application/json" } })
+                        .then(resp => {
+                            return resp.json()
+                        })
+                        .then(data => {
+                            //console.log(data);
+
+                            const attackName = data.name;
+                            document.querySelector("#attackName").innerHTML = attackName;
+
+                            const flavorText = data.flavor_text_entries[0].flavor_text;
+                            document.querySelector("#theFlavorText").innerHTML = flavorText;
+
+                            const accuracy = data.accuracy;
+                            document.querySelector("#accuracy").innerHTML = accuracy;
+
+                            const pp = data.pp;
+                            document.querySelector("#pp").innerHTML = pp;
+
+                            const power = data.power;
+                            document.querySelector("#power").innerHTML = power;
+
+                            const type = data.type.name;
+                            document.querySelector("#attackType").innerHTML = type;
+                        })
+
+                };
+                listElement.append(button);
+                myList.append(listElement);
+            }
+
+        }
+        ).catch(e => 
+            document.querySelector("#error").innerHTML="Pokemon does not exist."
+            )
+
+
+}
+
+function barChart(hp,atk,def,sAtk,sDef,speed){
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['HP', 'Attack', 'Defense', 'Sp Atk', 'Sp Def', 'Speed'],
+            datasets: [{
+                label: 'Base Stats',
+                data: [hp, atk, def, sAtk, sDef, speed],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function reset() {
+    document.querySelector("#attackName").innerHTML = "";
+    document.querySelector("#theFlavorText").innerHTML = "";
+    document.querySelector("#accuracy").innerHTML = "";
+    document.querySelector("#pp").innerHTML = "";
+    document.querySelector("#power").innerHTML = "";
+    document.querySelector("#attackType").innerHTML = "";
+
+    document.querySelector("#name").innerHTML = "";
+    document.querySelector("#image").innerHTML = "";
+    document.querySelector("#height").innerHTML = "";
+    document.querySelector("#weight").innerHTML = "";
+    document.querySelector("#types").innerHTML = "";
+
+    document.querySelector("#hp").innerHTML = "";
+    document.querySelector("#attack").innerHTML = "";
+    document.querySelector("#defense").innerHTML = "";
+    document.querySelector("#special-attack").innerHTML = "";
+    document.querySelector("#special-defense").innerHTML = "";
+    document.querySelector("#speed").innerHTML = "";
+
+    document.querySelector("#error").innerHTML = "";
+
+    const myList = document.querySelector("#move-list");
+    myList.innerHTML="";
+}
+
+
---
+++ Lab9/Part2&3/login.js
@@ -0,0 +1,66 @@
+﻿
+
+// function createUser(){
+//     fetch("https://spy-notes-api.rk0.xyz/users/",{method: "POST"})
+//     .then(resp=>resp.json())
+//     .then(data=>{
+//         console.log(data.userId);
+//         // window.history.pushState('userID=' + data.userId, 'userID', '?userID=' + data.userId); //adds url parameter
+//          window.location.href = 'index.html?userID='+data.userId; 
+//     })  
+// }
+
+function login(){
+    var user = document.querySelector("#userID");
+    window.location.href = 'index.html?userID='+user.value;
+}
+
+function register(){
+    fetch("https://spy-notes-api.rk0.xyz/users/",{method: "POST"})
+    .then(resp=>resp.json())
+    .then(data=>{
+        var x = document.querySelector("#credentials");
+        x.innerHTML+="Your user ID is " + "<strong>"+data.userId+"</strong>. Please save it somewhere safe as you will use it to login.";
+    })
+}
+
+function qrcode(){
+    var video = document.createElement("video");
+    var canvasElement = document.getElementById("canvas");
+    var canvas = canvasElement.getContext("2d");
+
+
+    // Use facingMode: environment to attemt to get the front camera on phones
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
+      video.srcObject = stream;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.play();
+      requestAnimationFrame(tick);
+    });
+
+    function tick() {
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        canvasElement.hidden = false;
+
+
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+        var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+          inversionAttempts: "dontInvert",
+        });
+        if (code) {
+          // console.log(code.data);
+          window.location.href = 'index.html?userID='+code.data;
+        }
+      }
+      requestAnimationFrame(tick);
+    }
+}
+
+
+
+// deleteNote("9ddf020f-214b-4ddd-9527-0ec642ac8d6b","9e6117cd-9b5f-4a27-8575-99fe7c2c3c40")
+//  createNote("9ddf020f-214b-4ddd-9527-0ec642ac8d6b");
+//  getNotes("9ddf020f-214b-4ddd-9527-0ec642ac8d6b");
